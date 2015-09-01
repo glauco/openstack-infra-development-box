c0bbfa5a0625a0b062dcd7afb4c5d0c7c25a8825
Glauco Oliveira <gvinici@thoughtworks.com>
2015-09-01 10:55:10 -0300

    Updating source.list

--- scripts/base.sh
+++ scripts/base.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-perl -p -i -e 's#http://us.archive.ubuntu.com/ubuntu#http://mirror.rackspace.com/ubuntu#gi' /etc/apt/sources.list
+perl -p -i -e 's#http://br.archive.ubuntu.com/ubuntu#gi' /etc/apt/sources.list
 
 # Update the box
 apt-get -y update >/dev/null
