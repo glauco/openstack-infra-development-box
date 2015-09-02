3b80275042ab29da902155add64ace37eb7e7329
Bruno Tavares <bruno.exz@gmail.com>
2015-09-02 12:32:33 -0300

    Installs the linux-headers-generic package to always install the headers when upgrading the kernel

--- scripts/base.sh
+++ scripts/base.sh
@@ -4,7 +4,7 @@ perl -p -i -e 's#http://br.archive.ubuntu.com/ubuntu#gi' /etc/apt/sources.list
 
 # Update the box
 apt-get -y update >/dev/null
-apt-get -y install facter linux-headers-$(uname -r) build-essential zlib1g-dev libssl-dev libreadline-gplv2-dev curl unzip >/dev/null
+apt-get -y install facter linux-headers-generic linux-headers-$(uname -r) build-essential zlib1g-dev libssl-dev libreadline-gplv2-dev curl unzip >/dev/null
 
 # Tweak sshd to prevent DNS resolution (speed up logins)
 echo 'UseDNS no' >> /etc/ssh/sshd_config
