da74162552b6c7ea5a5868fc044e7d71c56f776d
Bruno Tavares <connect+github@bltavares.com>
2015-10-20 16:35:55 -0200

    Creates the trusty image

---
+++ ubuntu-precise/scripts/base.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -x
+
+# Update the box
+rm -fR /var/lib/apt/lists/*
+
+apt-get -y update
+apt-get -y install facter build-essential zlib1g-dev libssl-dev libreadline-gplv2-dev curl unzip
+
+# Tweak sshd to prevent DNS resolution (speed up logins)
+echo 'UseDNS no' >> /etc/ssh/sshd_config
+
+# Remove 5s grub timeout to speed up booting
+cat <<EOF > /etc/default/grub
+# If you change this file, run 'update-grub' afterwards to update
+# /boot/grub/grub.cfg.
+
+GRUB_DEFAULT=0
+GRUB_TIMEOUT=0
+GRUB_DISTRIBUTOR=`lsb_release -i -s 2> /dev/null || echo Debian`
+GRUB_CMDLINE_LINUX_DEFAULT="quiet"
+GRUB_CMDLINE_LINUX="debian-installer=en_US"
+EOF
+
+update-grub
